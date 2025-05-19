787b541224d899152dfee701ae8cc183b4bd2297
Samin <samin99099@gmail.com>
2025-05-19 19:48:45 +0600

    Copy Sadman bhais project structure

--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -1,26 +1,7 @@
 package wal
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"wal/proto"
-
-	"google.golang.org/protobuf/proto"
-)
-
-func Add(name string, age string) {
-
-	wal := &proto.Wal{
-		Key:   name,
-		Value: age,
-	}
-
-	data, err := proto.Marshal(wal)
-	if err != nil {
-		log.Fatalf("Marshal error: %v", err)
-	}
-
-	fmt.Println("Serialized data: %s", data)
-	os.WriteFile("keyValue.bin", data, 0644)
+func Add(a int, b int) int {
+	return a + b
 }
+
+func Save()
