8d2250af2f35379f4f1a6a30ca94eec54a6e9758
Samin <samin99099@gmail.com>
2025-05-20 07:07:15 +0600

    Error message showing target does not provide the DeclaredToolchainInfo provider

--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -1,7 +1,31 @@
 package wal
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"simplewal/proto"
+
+	"google.golang.org/protobuf/proto"
+)
+
 func Add(a int, b int) int {
 	return a + b
 }
 
-func Save()
+func Save(key string, value string) string {
+
+	wal := &proto.WAL_Entry{
+		isCheckpoint: proto.Bool(true),
+	}
+
+	data, err := proto.Marshal(wal)
+	if err != nil {
+		log.Fatalf("Marshal error: %v", err)
+	}
+
+	fmt.Println("Serialized data: %s", data)
+	os.WriteFile("person.bin", data, 0644)
+
+	return "key: " + key + " value: " + value
+}
